77b35fddb3c4a42c10dc4d659598dbb33232da79
hbuffard <hugobuffard@live.fr>
2021-09-21 23:09:42 +0200

    Migrations + relations

---
+++ database/migrations/2021_09_21_205613_add_projet-id_to_images.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddProjetIdToImages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            //
+            $table->foreignId('projet_id')->constrained('projets')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('images', function (Blueprint $table) {
+            //
+        });
+    }
+}
